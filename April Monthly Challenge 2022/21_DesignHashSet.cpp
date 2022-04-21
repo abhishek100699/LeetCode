8cc5edcf2d6e0ae15c1493757608d587f3941d17
Abhishek Singh <abhishek100699@gmail.com>
2022-04-21 22:17:34 +0530

    designHashSet

---
+++ April Monthly Challenge 2022/21_DesignHashSet.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class MyHashSet {
+public:
+    vector<int> hash;
+    MyHashSet() {
+        hash.resize(1e6+1,false);
+    }
+    
+    void add(int key) {
+        hash[key]= true;
+    }
+    
+    void remove(int key) {
+        hash[key] = false;
+    }
+    
+    bool contains(int key) {
+        return hash[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
