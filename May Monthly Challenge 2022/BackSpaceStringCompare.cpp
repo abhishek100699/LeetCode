7d67d9ac0db95774beb963609d1882a68707266a
Abhishek Singh <abhishek100699@gmail.com>
2022-05-01 22:12:30 +0530

    BackspaceStringCompare

---
+++ May Monthly Challenge 2022/BackSpaceStringCompare.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i=0;
+        while(i<s.length()){
+            if(s[i]=='#'){
+                if(i==0){
+                    s.erase(i,1);
+                }
+                else{
+                    s.erase(i-1,2);
+                    i--;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        i=0;
+        
+        while(i<t.length()){
+            if(t[i]=='#'){
+                if(i==0){
+                    t.erase(i,1);
+                }
+                else{
+                    t.erase(i-1,2);
+                    i--;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return s==t;
+    }
+};
