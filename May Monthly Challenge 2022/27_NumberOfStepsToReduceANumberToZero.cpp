7390e6edf79855811799eb83290acc973421cdc5
Abhishek Singh <abhishek100699@gmail.com>
2022-05-27 18:42:55 +0530

    27may

---
+++ May Monthly Challenge 2022/27_NumberOfStepsToReduceANumberToZero.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int numberOfSteps(int num) {        
+        
+        int count = 0;
+        while(num){
+            if(num%2==0){
+                num = num/2;
+            }
+            else{
+                num = num-1;
+            }
+            count++;
+        }
+        return count;
+    }
+};
